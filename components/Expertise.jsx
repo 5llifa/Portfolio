1459a39a04ca8dd64c00ff035766f8c5be04d143
5llifa <5lifaals9@gmail.com>
2025-08-02 19:08:33 +0400

    Update Expertise.jsx
    Signed-off-by: 5llifa <5lifaals9@gmail.com>

--- components/Expertise.jsx
+++ components/Expertise.jsx
@@ -4,7 +4,7 @@ import swift from "../src/assets/swift.png";
 import github from "../src/assets/github.svg";
 import cplus from "../src/assets/cplus.png";    
 import xcode from "../src/assets/xcode.png";
-import css from "../src/assets/css.png";
+import css from "../src/assets/css.PNG";
 import javascript from "../src/assets/javascript.png";
 import java from "../src/assets/java.png";
 import csharp from "../src/assets/csharp.svg";
